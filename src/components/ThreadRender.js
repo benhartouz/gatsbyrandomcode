eec2b86f769f36b349d70d9a05a7861cea296235
Mohamed Ben hartouz <benhartouzmohamed@gmail.com>
2019-03-03 18:52:37 +0100

    add remark plugin

--- src/components/ThreadRender.js
+++ src/components/ThreadRender.js
@@ -1,10 +1,24 @@
-import React, { useContext } from "react";
-
+import React, { useContext, useEffect, useState } from "react";
 import { ThreadInputContainer } from "../state";
+import utf8 from "remark-utf8";
+import remark from "remark";
 
 const ThreadRender = () => {
     const { value } = useContext(ThreadInputContainer.Context);
-    console.log(value);
-    return value;
+    const [rendered, setRendered] = useState("");
+
+    useEffect(() => {
+        remark()
+            .use(utf8)
+            .process(value, (err, output) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    setRendered(output);
+                }
+            });
+    }, [value]);
+
+    return rendered;
 };
 export default ThreadRender;
