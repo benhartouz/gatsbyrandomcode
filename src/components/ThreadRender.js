d4a4848db1836b14fc30aa72338aed27abf45a0a
Mohamed Ben hartouz <benhartouzmohamed@gmail.com>
2019-03-03 20:09:42 +0100

    Fixe issues

--- src/components/ThreadRender.js
+++ src/components/ThreadRender.js
@@ -2,11 +2,18 @@ import React, { useContext, useEffect, useState } from "react";
 import { ThreadInputContainer } from "../state";
 import utf8 from "remark-utf8";
 import remark from "remark";
+import styled from "styled-components";
 
-const ThreadRender = () => {
-    const { value } = useContext(ThreadInputContainer.Context);
+const TweetStyle = styled.div`
+    font-size: 24px;
+`;
+
+const CharCount = ({ value }) => {
+    return <div>{value.length}</div>;
+};
+
+const Tweet = ({ value }) => {
     const [rendered, setRendered] = useState("");
-
     useEffect(() => {
         remark()
             .use(utf8)
@@ -14,11 +21,28 @@ const ThreadRender = () => {
                 if (err) {
                     console.log(err);
                 } else {
-                    setRendered(output);
+                    setRendered(output.contents);
                 }
             });
     }, [value]);
-
-    return rendered;
+    return (
+        <TweetStyle>
+            {rendered}
+            <CharCount value={rendered} />
+            sdfsd
+        </TweetStyle>
+    );
 };
+
+const ThreadRender = () => {
+    const { value } = useContext(ThreadInputContainer.Context);
+    return (
+        <>
+            {value.split("---").map(tweet => {
+                return <Tweet value={tweet} />;
+            })}
+        </>
+    );
+};
+
 export default ThreadRender;
